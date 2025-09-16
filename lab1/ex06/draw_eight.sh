c54d629e71d148bd991631cbcfdbced7e6605afd
Michael Vays <michailvays@gmail.com>
2025-09-16 13:22:52 +0700

    added lab1/ex06 subtask folder

---
+++ lab1/ex06/draw_eight.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Source ROS 2
+source /opt/ros/jazzy/setup.bash
+
+# Publish left turn for 4 seconds
+ros2 topic pub --rate 1 /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 1.8}}" &
+sleep 4
+kill $!
+
+# Publish right turn for 4 seconds
+ros2 topic pub --rate 1 /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -1.8}}" &
+sleep 4
+kill $!
+
+# OPTIONAL: repeat for a more defined figure-eight
+ros2 topic pub --rate 1 /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 1.8}}" &
+sleep 4
+kill $!
+
+ros2 topic pub --rate 1 /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -1.8}}" &
+sleep 4
+kill $!
